222ae9b317343c9ab750b782102c18dc3dd534b1
AgsalAee <agsal.agsal9@gmail.com>
2020-09-09 18:13:22 +0700

    update 3rd page

--- Team6/app/src/main/java/com/example/team6/MainActivity.java
+++ Team6/app/src/main/java/com/example/team6/MainActivity.java
@@ -8,13 +8,12 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Button button;
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener(){
             @Override
--- Team6/app/src/main/java/com/example/team6/Page2.java
+++ Team6/app/src/main/java/com/example/team6/Page2.java
@@ -2,13 +2,20 @@ package com.example.team6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Page2 extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
     }
+    public void button(View view) {
+        Intent i = new Intent(this, MainActivity3.class);
+        startActivity(i);
+    }
 }
+
